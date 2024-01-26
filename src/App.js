6863e9e287da1271aef5584d6ef242fb7711134b
PriyaTharsini702 <priyatharsini702@gmail.com>
2024-01-26 15:32:17 +0530

    committed

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Main from "./components/Main";
 import Signup from "./components/Singup";
 import Login from "./components/Login";
-import Addproduct from  "./components/Addproduct/addproduct";
-import Update from "./components/Updateproduct/update";
 
 function App() {
 	const user = localStorage.getItem("token");
@@ -15,9 +13,7 @@ function App() {
 			<Route path="/login" exact element={<Login />} />
 			<Route path="/" element={<Navigate replace to="/login" />} />
 			<Route path="/home" element={<Navigate replace to="/login" />} />
-			<Route path="/update" element={<Navigate replace to="/home" />} />
-			{user && <Route path="/addproduct" element={<Addproduct/>} /> }
-			{user && <Route path="/update/:_id" element={<Update/>} /> }
+			
 		</Routes>
 	);
 }
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,101 +1,165 @@
 import styles from "./styles.module.css";
-import { useEffect, useState } from "react";
-import { useNavigate, Link} from "react-router-dom";
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
 
 const Main = () => {
-	const history = useNavigate()
-	const [logout, setlogout] = useState(false);
-	const [data, setdata] = useState([])
+	const [selectedSeats, setSelectedSeats] = useState([]);
+
+	const [count, setCount] = useState(0);
+
+	const [ticket, setTicket] = useState(0);
+	const [selectedMovieIndex, setSelectedMovieIndex] = useState(0);
+	const [ticketPrice, setTicketPrice] = useState(220);
+	const [movies, setMovies] = useState([
+		{ title: 'Godzilla vs Kong', price: 220 },
+		{ title: 'Radhe', price: 320 },
+		{ title: 'RRR', price: 250 },
+		{ title: 'F9', price: 260 },
+	]);
 
 	useEffect(() => {
+			const storedSelectedArrays = localStorage.getItem('selectedSeatsArray');
+			if(storedSelectedArrays){
+			populateUI();
+		}
+		const storedSelectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+		if (storedSelectedMovieIndex) {
+			setSelectedMovieIndex(parseInt(storedSelectedMovieIndex, 10));
+		}
+	}, []);
 
-		if (!localStorage.getItem('token'))
-			history.location = "/login";
 
-		axios.get('https://crudcrud.com/api/c901b5bf0c1a46ef9d3c41990052223a/productlists')
-			.then(res => {
-				setdata(res.data);
-				console.log(res.data)
-			})
-			.catch((err) => console.error(err))
+	useEffect(() => {
+		const storedSelectedArrays = localStorage.getItem('selectedSeatsArray');
+		if (storedSelectedArrays) {
+			populateUI();
+		}
 
-	}, [logout])
+	}, [selectedSeats])
 
-	const handleLogout = (e) => {
-		e.preventDefault();
-		localStorage.removeItem("token");
-		window.location.reload();
-		setlogout(true)
+	const populateUI = () => {
+		debugger
+		const selectedSeatsArrayString = localStorage.getItem('selectedSeatsArray');
+		const selectedSeatsArray = JSON.parse(selectedSeatsArrayString)
+		console.log(selectedSeatsArray);
+
+		selectedSeatsArray.forEach((seatIndex) => {
+				const rowElement = document.querySelector(`.seat:nth-child(${seatIndex + 1})`);
+				if (rowElement) {
+				  rowElement.classList.add(styles.sold);
+				}
+		});
+
+	}
+
+	const setMovieData = (movieIndex, moviePrice) => {
+		localStorage.setItem('selectedMovieIndex', movieIndex);
+		localStorage.setItem('selectedMoviePrice', moviePrice);
+	};
+
+	const updateSelectedCount = () => {
+		debugger
+
+		const selectedSeatsCount = selectedSeats.length;
+
+		const totalPrice = selectedSeatsCount * ticketPrice;
+
+		setMovieData(selectedMovieIndex, ticketPrice);
+
+		localStorage.setItem('selectedSeats', selectedSeatsCount);
 
 	};
 
-	 const handleDelete = (id)=>{
-		const confirm = window.confirm("Are you sure, want to delete?")
+	const handleMovieChange = (e) => {
+		debugger
+		setTicketPrice(+e.target.value);
+		setSelectedMovieIndex(e.target.selectedIndex);
+		setMovieData(e.target.selectedIndex, e.target.value);
+updateSelectedCount()		
+};
 
-		if(confirm){
-			axios.delete(`https://crudcrud.com/api/c901b5bf0c1a46ef9d3c41990052223a/productlists/${id}`)
-			.then(res=> {
-				history.location="/home";
-				 window.location.reload();})
-			.catch(err=>console.log(err))
+	const handleSeatClick = (e, index) => {
+		debugger
+		console.log(e.target)
+		if (e.target.classList.contains('seat') && !e.target.classList.contains('sold')) {
+			const updatedSelectedSeats = [...selectedSeats];
+			const seatIndex = updatedSelectedSeats.indexOf(index);
+
+			if (seatIndex > -1) {
+				updatedSelectedSeats.splice(seatIndex, 1);
+			} else {
+				updatedSelectedSeats.push(index);
+			}
+
+			setSelectedSeats(updatedSelectedSeats);
+			updateSelectedCount();
 		}
-	 }
+	};
+
+	const handleBookButtonClick = () => {
+		debugger
+		console.log(selectedSeats)
+
+		let seatsarray = [];
+		selectedSeats.forEach((seatIndex) => {
+			seatsarray.push(seatIndex)
+		});
+
+		console.log(seatsarray)
+		setSelectedSeats([]);
+		localStorage.setItem('selectedSeatsArray', JSON.stringify(seatsarray));
+		updateSelectedCount();
+		console.log(localStorage.getItem('selectedSeatsArray'))
+
+	};
+
 
 	return (
-		<div className={styles.main_container}>
-			<nav className={styles.navbar}>
-				<h1 >Shoppyfy</h1>
-				<button className={styles.white_btn} onClick={(e) => handleLogout(e)}>
-					Logout
-				</button>
-			</nav>
-			<div className={styles.imageflex}>
-				<div className={styles.bgimage} />
-				<div className={styles.bgimage1} />
-				<div className={styles.bgimage2} />
-			</div>
-			<br /><br />
-			<p className={styles.heading}>LIST OF PRODUCTS</p>
-			<br /><br />
-
-			<div style={{width: '90%', textAlign:'right'}}>
-			<button className={styles.add}><Link to='/addproduct' style={{textDecoration:'none'}}>Add Product +</Link></button>
-			</div>
-			<br/>
-			<div className={styles.tablemain}>
-				<table className={styles.tablestyle}>
-					<thead>
-						<tr>
-							<th>Id</th>
-							<th>Product Name</th>
-							<th>Price</th>
-							<th>Description</th>
-							<th>Action</th>
-						</tr>
-					</thead>
-					<tbody>
-						{Array.isArray(Object.values(data)) ? (
-							Object.values(data).map((item) => (
-								<tr>
-									<td>{item.id}</td>
-									<td>{item.name}</td>
-									<td>{item.price}</td>
-									<td>{item.description}</td>
-									<td>
-										<button className={styles.edit}> <Link to={`/update/${item._id}`} style={{textDecoration:'none'}}>Edit</Link></button>
-										<button onClick={(e)=>{handleDelete(item._id)}} className={styles.delete}>Delete</button></td>
-								</tr>
-							))
-						) : (
-							<tr>
-								<td colSpan="5">No data available</td>
-							</tr>
-						)}
-					</tbody>
-				</table>
+		<div>
+			<div className={styles.moviecontainer}>
+				<label> Select a movie:</label>
+				<select value={movies[selectedMovieIndex].price} onChange={handleMovieChange}>
+					{movies.map((movie, index) => (
+						<option key={index} value={movie.price}>
+							{movie.title} (RS.{movie.price})
+						</option>
+					))}
+				</select>
 			</div>
 
+			<ul className={styles.showcase}>
+				<li>
+					<div className={styles.seat}></div>
+					<small>Available</small>
+				</li>
+				<li>
+					<div className={styles.selected}></div>
+					<small>Selected</small>
+				</li>
+				<li>
+					<div className={styles.sold}></div>
+					<small>Sold</small>
+				</li>
+			</ul>
+			<div className={styles.container}>
+				<div className={styles.screen}></div>
+
+				{Array.from({ length: 6 }, (_, rowIndex) => (
+					<div key={rowIndex} className={styles.row}>
+						{Array.from({ length: 8 }, (_, seatIndex) => (
+							<div
+								key={seatIndex}
+								className={`seat ${selectedSeats.includes(seatIndex + rowIndex * 8) ? `${styles.selected}` : `${styles.seat}`} `}
+								onClick={(e) => handleSeatClick(e, seatIndex + rowIndex * 8)}
+							></div>
+						))}
+					</div>
+				))}
+			</div>
+			<p className={styles.text}>
+				You have selected <span id="count">{selectedSeats.length !== 0 ? selectedSeats.length : count}</span> seat for a price of RS.
+				<span id="total">{selectedSeats.length !== 0 ? selectedSeats.length * ticketPrice : ticket}</span>
+			</p>
+			<button onClick={handleBookButtonClick}>Book</button>
 		</div>
 	);
 };
